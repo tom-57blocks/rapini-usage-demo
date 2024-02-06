2a803a01af6b0f90295a035487868600cb903307
Tom Liu <tom.liu@57blocks.com>
2024-02-06 16:56:03 +0800

    demo

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -3,7 +3,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as YAML from 'yaml';
 import * as fs from 'fs';
-import swaggerUi from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -26,18 +25,10 @@ async function bootstrap() {
 
   const yamlString: string = YAML.stringify(document, {});
   console.log('yamlString:', yamlString);
-  fs.writeFileSync('./with-extensions.yaml', yamlString);
+  fs.writeFileSync('./swagger-open-api.yaml', yamlString);
 
-  const file = fs.readFileSync('./with-extensions.yaml', 'utf8');
-  // const swaggerDocument = YAML.parse(file)
+  const file = fs.readFileSync('./swagger-open-api.yaml', 'utf8');
 
-  // app.use('/api-yaml', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-  //   app.get('/myfile', (req:any, res:any) => {
-  //     res.sendFile("./with-extensions.yaml");
-  //  });
-
-  // await app.listen(process.env.NEST_PORT);
   await app.listen(3001);
 }
 bootstrap();
